b0af42b7dd2c6095171ab34152bf5c5aeb2cec3d
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2022-09-28 10:42:26 +0200

    fix compatiblity of PrimeMongoDbServiceProvider on prime >= v1.0.2

--- src/MongoDB/PrimeMongoDbServiceProvider.php
+++ src/MongoDB/PrimeMongoDbServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Bdf\Prime\MongoDB;
 
+use Bdf\Prime\Connection\Factory\ConnectionFactory;
+use Bdf\Prime\ConnectionManager;
 use Bdf\Prime\MongoDB\Driver\MongoConnection;
 use Bdf\Prime\MongoDB\Driver\MongoDriver;
 use Bdf\Web\Application;
@@ -9,6 +11,8 @@ use Bdf\Web\Providers\ServiceProviderInterface;
 
 /**
  * Configuration de prime connection manager afin d'ajouter le map du driver
+ *
+ * @deprecated Do nothing since prime v1.1.0, will be deleted on v2.0.0
  */
 class PrimeMongoDbServiceProvider implements ServiceProviderInterface
 {
@@ -17,13 +21,18 @@ class PrimeMongoDbServiceProvider implements ServiceProviderInterface
      */
     public function configure(Application $app)
     {
-        /**
-         * ConnectionManager
-         */
-        $app->extend('prime-connectionManager', function ($connections, $app) {
-            $connections->registerDriverMap('mongodb', MongoDriver::class, MongoConnection::class);
+        // Prime <= 1.0.1
+        if (method_exists(ConnectionManager::class, 'registerDriverMap')) {
+            $app->extend('prime-connectionManager', function ($connections, $app) {
+                $connections->registerDriverMap('mongodb', MongoDriver::class, MongoConnection::class);
 
-            return $connections;
-        });
+                return $connections;
+            });
+        }
+
+        // Prime = v1.0.2
+        if (class_exists(ConnectionFactory::class) && ConnectionFactory::getDriverMap('mongodb')) {
+            ConnectionFactory::registerDriverMap('mongodb', MongoDriver::class, MongoConnection::class);
+        }
     }
 }
